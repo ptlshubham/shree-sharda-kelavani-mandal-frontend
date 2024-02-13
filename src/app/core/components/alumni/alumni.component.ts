71d1cd4ff199dc74437cdd10b587c35fbd310f6d
Meet Kachhiya <meet@gmail.com>
2024-02-13 17:51:55 +0530

    Alumni Registration,about us ,our history changes

--- src/app/core/components/alumni/alumni.component.ts
+++ src/app/core/components/alumni/alumni.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-alumni',
@@ -8,10 +9,41 @@ import { Component } from '@angular/core';
 export class AlumniComponent {
   Menuoption = 'center';
   Settingicon = true;
+  years: number[] = [];
+  validationForm!: FormGroup;
+  yournameModel: any= {};
   
-  constructor() { }
+  constructor(
+    public formBuilder: UntypedFormBuilder
+  ) {}
 
   ngOnInit(): void {
-
+    this.validationForm = this.formBuilder.group({
+      yourname: ['',[Validators.required]],
+      alumni: ['',[Validators.required]],
+      subject: ['',[Validators.required]],
+      year: ['',[Validators.required]],
+      email: ['',[Validators.required]],
+      call: ['',[Validators.required]],
+    });
+    const currentYear = new Date().getFullYear();
+    const startYear =1955;
+    for (let year = startYear; year <= currentYear; year++) {
+      this.years.push(year);
+    }
   }
+
+  get f() {return this.validationForm.controls;}
+  populateYearDropdown(): void {
+    const select = document.getElementById("yearDropdown") as HTMLSelectElement;
+    const currentYear = new Date().getFullYear();
+    const startYear = 1955;
+
+    for (let i = currentYear; i >= startYear; i--) {
+        const option = document.createElement("option");
+        option.text = i.toString();
+        option.value = i.toString();
+        select.add(option);
+    }
+}
 }
